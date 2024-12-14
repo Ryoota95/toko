9ef67ba8fb964a01229b8e75a94a3aea971fc641
aby <rayyadabiyazid@gmail.com>
2024-12-14 02:27:18 -0800

    versi 1 barnag dan delete proses

---
+++ delete_proses.php
@@ -0,0 +1,12 @@
+<?php
+     include 'koneksi.php';
+     if (isset($_GET['kode'])) {
+     	mysqli_query($koneksi, "delete from nama_barang where id_barang = '$_GET[kode]'");
+
+    	echo "data berhasil dihapus XD";
+
+     	header("refresh:0;url=http://localhost/toko/barang.php");
+     }
+
+ ?>
+ 
